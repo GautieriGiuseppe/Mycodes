6739d184911a0d53f9e87432be70358455d21a46
Giuseppe <giuseppe.gautieri00@gmail.com>
2025-03-18 08:54:02 +0100

    Delete My_projects/Long-Reads_Analyzer/Long_reads_Analyzer.py

--- My_projects/Long-Reads_Analyzer/Long_reads_Analyzer.py
+++
@@ -1,9 +0,0 @@
-from Bio import SeqIO
-import subprocess
-
-""" Example fastq composition per sequence, many in the same file
-@SRR077391.1 HWUSI-EAS667_105020215:3:1:1339:1030/2
-TCCCTTTCTACTTAAATCTTGGTTGAGTTTAGGTGTCATTGTTTTGTATACCTACTGAATCCATATTTGTCTGCATCAGAGCACATTATGACATCTGGGT
-+
-G3BGBGE@===?4BB<>59ABCA=4-=B?B<EB:D8+,==BBA87+<+@6BD8-C%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-"""
